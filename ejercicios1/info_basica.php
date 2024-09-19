f2f113b23acbfe782cd005b197cbbbf9364eb32b
Jacka97 <byJacka@gmail.com>
2024-09-19 18:29:42 +0200

    Hola

---
+++ ejercicios1/info_basica.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+<h1>Página de prueba en PHP</h1>
+<?php
+// Variables para almacenar el nombre y el año actual
+$nombre = "Daniel Zaragoza";
+$anyo = 1997;
+?>
+<p>Soy <?php echo $nombre; ?> y nací en el año <?php echo $anyo; ?>.</p>
+</body>
+</html>
