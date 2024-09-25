8b1f973d74c8fd713315d6dfd21cc2b75e4725ba
Jacka97 <byJacka@gmail.com>
2024-09-25 14:55:29 +0200

    Ejercicios 2

---
+++ ejercicios2/contador_funciones.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <h1>Contador Funciones</h1>
+    <?php
+    $a = 10;
+    $b = 20;
+    function bucleContar($inicio, $fin){
+        for($i = $inicio; $i <= $fin; $i++){
+            if($i == $fin){
+                echo " $i";
+            }else{
+                echo "$i, ";
+            }
+        }
+    }
+    function cuenta($a, $b){
+    if($b > $a){
+        bucleContar($a, $b);
+    }elseif ($a > $b){
+        bucleContar($b, $a);
+    }else{
+        echo "Son iguales!";
+    }
+    }
+    cuenta($b, $a);
+    ?>
+</body>
+</html>
---
+++ ejercicios2/saludo_funciones.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <h1>Saludo Funciones</h1>
+    <?php
+    $nombre =  "Nacho";
+    function saludo($a)
+    {
+    echo "Hola $a";
+    }
+    saludo($nombre)
+    ?>
+</body>
+</html>
