e1380cdd2ebc333a9df069057f859d3052346a97
Jacka97 <byJacka@gmail.com>
2024-09-25 14:54:53 +0200

    Actualizaciones minimas

---
+++ ejercicios1/variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+<?php
+$num1 = 3;
+$num2 = 5;
+$num3 = 8;
+$num1 *= 4;
+echo $num1;
+echo $num1 <= $num2;
+echo $num3 > $num1 and $num3 > $num2;
+echo $num3 > $num1 or $num3 > $num2;
+echo $num1 > $num2 xor $num1 > $num3;
+$num3--;
+echo $num3;
+$num3 += $num1;
+echo $num3;
+?>
+</body>
+</html>
