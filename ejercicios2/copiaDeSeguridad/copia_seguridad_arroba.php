f5e18311500c6d5f221cdd89388c3a6610fdb500
Jacka97 <byJacka@gmail.com>
2024-10-02 15:39:43 +0200

    Copia de Seguridad

---
+++ ejercicios2/copiaDeSeguridad/copia_seguridad_arroba.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 9</title>
+</head>
+<body>
+    <h1>Copia de Seguridad</h1>
+    <?php
+        $fichero = "datos1.txt";
+        $ficheroCopia = "copiaDatos.txt";
+
+            $contenido = @file_get_contents($fichero);
+            file_put_contents($ficheroCopia, $contenido);
+            echo "Copia de seguridad creada con exito";
+
+        
+    ?>
+</body>
+</html>
