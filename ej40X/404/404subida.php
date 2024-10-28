dfa7e55e57193a47b8784aa8079819706a8c307c
Jacka97 <byJacka@gmail.com>
2024-10-28 18:20:51 +0100

    Ejercicios

---
+++ ej40X/404/404subida.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Muestra Imagen</title>
+</head>
+<body>
+<?php
+$dimesionX = $_POST["dimensionX"];
+$dimesionY =$_POST["dimensionY"];
+if (isset($_POST['btnSubir']) && $_POST['btnSubir'] == 'Subir') {
+    if (is_uploaded_file($_FILES['archivo']['tmp_name'])) {
+        // subido con éxito
+        $nombre = "imagen_subida.jpg";
+        move_uploaded_file($_FILES['archivo']['tmp_name'], "./uploads/{$nombre}");
+
+        echo "<p>Archivo $nombre subido con éxito</p>";
+        echo "<img src='./uploads/$nombre' alt='Imagen subida'width='$dimesionX' heigh='$dimesionY'>";
+    }
+}
+
+    
+
+?>
+
+
+
+
+</body>
+</html>
